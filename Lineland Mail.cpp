09ea340d09d056a6fe2bb28de85ee1c3fc82b494
Kevin-Atef <kevinatef2006@gmail.com>
2021-08-18 17:43:42 +0200

    Add files

---
+++ Lineland Mail.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define fast() ios_base::sync_with_stdio(false);cin.tie(NULL);
+int main() {
+    fast();
+    int n;
+    cin >> n;
+    int cord[n];
+    for(int i = 0; i < n; i++) {
+        cin >> cord[i]; 
+    }
+    for(int i = 0; i < n; i++) {
+        int dis[n-1];
+        int z = 0;
+        for(int j = 0; j < n; j++) {
+            if(j == i) {
+                continue;
+            }
+            dis[z] = abs(cord[i] - cord[j]);
+            z++;
+        }
+        sort(dis, dis + (n -1));
+        cout << dis[0] << " " << dis[n - 2] << endl;
+    }
+    return 0;
+}
