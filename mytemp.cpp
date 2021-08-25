3b9193b08b564b63a55054ecdf548ee0b382ac94
Kevin-Atef <kevinatef2006@gmail.com>
2021-08-25 05:37:05 +0200

    My Template!

---
+++ mytemp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for (int i = a; i < b; i++)
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("", "r", stdin);
+    //freopen("", "w", stdout);
+
+    //solve();
+
+    /*int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }*/
+
+    return 0;
+}
